45d7dc2c92139bc257873e1f7466d8625a632a5a
VidgarVii <vidasp@mail.ru>
2018-11-05 23:15:41 +0300

    fix deiscriminant

--- leasson_1/discriminant.rb
+++ leasson_1/discriminant.rb
@@ -1,29 +1,25 @@
-# d = b**2-4ac
-# x = -(b)/2*a
-# x1 = (-(b)+Math.sqrt(d))/2*a
-# x2 = (-(b)-Math.sqrt(d))/2*a
-
 puts 'Научная компания "Рога и костыли" приветствует Вас'
 puts 'На этот раз мы будем вычислять дискриминант и корни уравнения'
 puts 'Введите 3 числа'
 print 'a = '
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 print 'b = '
-b = gets.chomp.to_i
+b = gets.chomp.to_f
 print 'c = '
-c = gets.chomp.to_i
-puts ''
+c = gets.chomp.to_f
 
-d = b**2-4*a*c
-puts d
+d = b ** 2 - 4 * a * c
 
-if d > 0
-  x1 = (-(b)+Math.sqrt(d))/2*a
-  x2 = (-(b)-Math.sqrt(d))/2*a
-  puts "Дискриминант - #{d}\n 2 корня:\n x1 = #{x1.round(2)}\n x2 = #{x2.round(2)}"
-elsif d == 0
-  x = -(b)/2*a
-  puts "Дискриминант - #{d}\n 1 корень - #{x}"
-else  
-  puts "Корней нет"
+if d < 0
+  puts "\n Корней нет"
+  abort
+else
+  x1 = ( - ( b ) + Math.sqrt( d ) ) / 2 * a
+  x2 = ( - ( b ) - Math.sqrt( d ) ) / 2 * a
+end
+
+if x1 == x2
+  puts "\n Дискриминант - #{d} \n 1 корень - #{x1}"
+else
+  puts "\n Дискриминант - #{d} \n 2 корня:\n x1 = #{x1.round(2)}\n x2 = #{x2.round(2)}"
 end

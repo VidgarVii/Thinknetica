442dc5183725693d0f8943005f7e2409c190e56b
VidgarVii <vidasp@mail.ru>
2018-11-06 00:00:33 +0300

    refactoring pifagor

--- leasson_1/pifagor.rb
+++ leasson_1/pifagor.rb
@@ -1,15 +1,5 @@
 triangle = []
 
-def pifagor (triangle)
-  triangle.map!{ |x| (x**2)  }
-  c = triangle.max
-  triangle.delete c
-  p triangle
-  p triangle.sum
-
-  puts 'Прямоуго́льный треуго́льник' if c == triangle.sum 
-end  
-
 puts 'Научная компания "Рога и костыли" приветствует Вас'
 puts 'Мы поможем Вам определить тип Вашего треугольника'
 puts 'Введите длину каждой стороны треугольника'
@@ -19,18 +9,32 @@ puts '   a /   \ b '
 puts '    /_____\ '
 puts '       c '
 print 'a = '
-triangle << gets.chomp.to_i
+triangle << gets.chomp.to_f
 print 'b = '
-triangle << gets.chomp.to_i
+triangle << gets.chomp.to_f
 print 'c = '
-triangle << gets.chomp.to_i
+triangle << gets.chomp.to_f
+
 check = triangle.uniq
 
-case check.size
-when 2 
-  puts 'Ого Ваш треугольник является равнобедренным!'
-  pifagor (triangle)
-when 1 then puts 'Ваш треугольник является равнобедренным и равносторонним, но не прямоугольный.' 
-else
-  pifagor (triangle)
+def pifagor? (triangle)
+  triangle.map! { |x| ( x ** 2 ) }
+  c = triangle.max
+  triangle.delete c
+
+  c == triangle.sum
+end 
+
+def isosceles? (triangle)
+  check = triangle.uniq
+  check.size == 2  
 end
+
+def rectangular? (triangle)
+  check = triangle.uniq
+  check.size == 1
+end
+
+puts 'Ваш треугольник является равнобедренным и равносторонним, но не прямоугольный.' if rectangular?(triangle)
+puts 'Ваш треугольник является равнобедренным!' if isosceles?(triangle)
+puts 'Прямоуго́льный треуго́льник' if pifagor?(triangle)

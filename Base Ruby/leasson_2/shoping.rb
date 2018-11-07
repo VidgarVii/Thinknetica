d27b3095fd0ffe3fae2cef2613a19431b87bdfad
Pavel <vidasp@mail.ru>
2018-11-07 13:43:22 +0300

    shoping v.2.0

--- Base Ruby/leasson_2/shoping.rb
+++ Base Ruby/leasson_2/shoping.rb
@@ -1,62 +1,24 @@
-class Item
-  attr_accessor :name, :price
+ 
+cart = {}
 
-  def initialize (name, price)
-    self.name, self.price = name.to_s, price.to_f
+def total_price (cart)
+  check = 0
+  cart.each_value do |item|    
+    check += item[:price] * item[:count]
   end  
+  return check
 end
 
-class Cart  
-  def initialize *item
-    @cart = []   
-  end
-
-  def add (item, count)
-    @cart << [item, count]
-  end
-  
-  def look
-    @cart
-  end
+loop do
+  puts 'Что покупаем? (Когда закончите покупки напишите "stop")'
+  name = gets.chomp.downcase
+  break if name == 'stop'
+  puts 'По какой цене?'
+  price = gets.chomp.to_f
+  puts 'Кол-во'
+  count = gets.chomp.to_f  
+  cart[name.to_sym] = {price: price, count: count}
 end
 
-class Store  
-  def initialize
-    @cart = Cart.new    
-    pay
-    check 
-  end
-  
-  def pay
-    loop do
-      puts 'Что покупаем? (Когда закончите покупки напишите "stop")'
-      name = gets.chomp.downcase
-      break if name == 'stop'
-
-      puts 'По какой цене?'
-      price = gets.chomp.to_f
-
-      puts 'Кол-во'
-      count = gets.chomp.to_f
-      item = Item.new(name, price)
-      @cart.add( item, count )     
-    end
-  end
-
-  def check # name => {price => count}, .....
-    check = {}
-    total = 0
-    puts 'Товар : Цена : Кол-во'
-    @cart.look.each do |item, count|
-      check[item.name] = { item.price => count }  
-      
-      puts "#{item.name}:  #{item.price} * #{count}"
-
-      total += item.price * count
-    end
-    puts "Итого к оплате: #{total}"
-    puts check
-  end
-end
-
-Store.new
+p cart
+puts "Общая цена: #{total_price(cart)}"

db039c6dadfbbbcb35def7f867e4eff948211db2
Pavel <vidasp@mail.ru>
2018-11-07 14:04:33 +0300

    fibonachi v.2

--- Base Ruby/leasson_2/fibonachi.rb
+++ Base Ruby/leasson_2/fibonachi.rb
@@ -1,10 +1,11 @@
 puts "Задача 3 Фибоначи" #0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
 
 fibonachi = [0, 1]
- 
-fibonachi.each_with_index do |single, index|
-  if single < 100
-    fibonachi[index + 1] = single + fibonachi[index - 1]
-  end
+
+loop do 
+  next_element = fibonachi[-2] + fibonachi[-1]
+  break if next_element > 100
+  fibonachi << next_element
 end
+
 p fibonachi

69c7ece836286fc7369a568f5a159e8056b81dbe
VidgarVii <vidasp@mail.ru>
2018-11-08 15:18:38 +0300

    type train for Station

--- Base Ruby/leasson_3_OOP/station.rb
+++ Base Ruby/leasson_3_OOP/station.rb
@@ -2,7 +2,7 @@
 + Имеет название, которое указывается при ее создании
 + Может принимать поезда (по одному за раз)
 + Может возвращать список всех поездов на станции, находящиеся в текущий момент
-Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
++ Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
 + Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции). 
 =end
 require_relative 'train'
@@ -23,16 +23,20 @@ class Station
     @list_train.delete(train)
   end
 
-  def list_train_for_type (type)
-    @list_train.reject{ |train| train.type != type }
+  def list_train_for_type (type)    
+    trains = @list_train.reject{ |train| train.type != type }
+    puts "#{trains.size} поезд(а,ов)"
+    trains
   end  
 end
 
 
 train = Train.new('01', 'freight', 10)
 train2 = Train.new('02', 'passenger', 15)
+train3 = Train.new('03', 'passenger', 15)
 state = Station.new 'A'
 state.add_train(train)
 state.add_train(train2)
-p state.list_train_for_type('freight')
+state.add_train(train3)
+p state.list_train_for_type('passenger')
 #p state.list_train

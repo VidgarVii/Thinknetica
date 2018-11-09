810a45892a37fe973d97e29fac2f33d4da122054
VidgarVii <vidasp@mail.ru>
2018-11-09 14:16:17 +0300

    refact alfavit

--- Base Ruby/leasson_2_H&A/alfavit.rb
+++ Base Ruby/leasson_2_H&A/alfavit.rb
@@ -1,11 +1,7 @@
-puts "Задача 4"
+puts 'Задача 4'
 
-check = %w(а е и я ю э у ы о)
+check = %w[а е и я ю э у ы о]
 alfavit = ('а'..'я').to_a
 total = {}
-alfavit.each_with_index { |k, i| 
-  if check.include?(k)
-    total[k] = i+1
-  end
-}
+alfavit.each_with_index { |k, i| total[k] = i + 1 if check.include?(k) }
 p total

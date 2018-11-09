dd850ee49c2986e7113f64f470b8373cb001271c
VidgarVii <vidasp@mail.ru>
2018-11-09 14:36:02 +0300

    refact calendar

--- Base Ruby/leasson_2_H&A/calendar.rb
+++ Base Ruby/leasson_2_H&A/calendar.rb
@@ -1,15 +1,14 @@
 month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-puts "Введите дату"
-print "День:"
+puts "Введите дату \nДень:"
 day = gets.chomp.to_i
-print "Месяц:"
+print 'Месяц:'
 month = gets.chomp.to_i
-print "Год:"
+print 'Год:'
 year = gets.chomp.to_i
 
-is_leap = ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 )
+is_leap = ((year % 4).zero? && year % 100 != 0 || (year % 400).zero?)
 month_days[1] = 29 if is_leap
 
-day_number = month_days.take(month - 1).sum + day 
+day_number = month_days.take(month - 1).sum + day
 puts "Прошло #{day_number} дня(ей)"

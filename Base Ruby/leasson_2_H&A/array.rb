73b49072eccb345912fcb21b9b94f7522241d45b
VidgarVii <vidasp@mail.ru>
2018-11-09 14:31:49 +0300

    refact array

--- Base Ruby/leasson_2_H&A/array.rb
+++ Base Ruby/leasson_2_H&A/array.rb
@@ -1,5 +1,5 @@
-puts "Задача 2"
-array = (10..100).to_a.reject{ |x| x%5 != 0 } 
+puts 'Задача 2'
+array = (10..100).to_a.select { |x| (x % 5).zero? }
 p array
 array2 = (10..100).step(5).to_a
 p array2

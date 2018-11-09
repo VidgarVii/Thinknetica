5c54e8d3553a5583fdc298f9ffcc0627ef75f035
Pavel <vidasp@mail.ru>
2018-11-10 00:07:42 +0300

    mkdir lib

--- Base Ruby/lesson_3_OOP/main.rb
+++ Base Ruby/lesson_3_OOP/main.rb
@@ -1,23 +1,11 @@
-require_relative 'train'
-require_relative 'route'
-require_relative 'station'
-require_relative 'train_cargo'
-require_relative 'train_passenger'
-require_relative 'wagon'
-require_relative 'wagon_cargo'
-require_relative 'wagon_passenger'
-require_relative 'adapter'
+require_relative 'lib/train'
+require_relative 'lib/route'
+require_relative 'lib/station'
+require_relative 'lib/train_cargo'
+require_relative 'lib/train_passenger'
+require_relative 'lib/wagon'
+require_relative 'lib/wagon_cargo'
+require_relative 'lib/wagon_passenger'
+require_relative 'lib/adapter'
 
-st1 = Station.new('Хогвартс')
-st2 = Station.new('Дурмстранг')
-st3 = Station.new('Шармбатоне')
-st4 = Station.new('Кастелобрушу')
-route = Route.new(st1, st4)
-route.add_station(st2, 1)
-route.add_station(st3, 2)
-route.puts_stations
-
-wp = PassengerWagon.new
-wc = CargoWagon.new
-tp = PassengerTrain.new '001'
-tc = CargoTrain.new '002'
+Adapter.new

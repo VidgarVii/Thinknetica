5166ec1e8202ed22b67b91b79350f3c3708318bf
VidgarVii <vidasp@mail.ru>
2018-11-09 14:51:25 +0300

    refact shoping

--- Base Ruby/leasson_2_H&A/shoping.rb
+++ Base Ruby/leasson_2_H&A/shoping.rb
@@ -1,23 +1,23 @@
- 
 cart = {}
 
 def total_price(cart)
   check = 0
-  cart.each_value do |item|    
+  cart.each_value do |item|
     check += item[:price] * item[:count]
-  end  
-  return check
+  end
+  check
 end
 
 loop do
   puts 'Что покупаем? (Когда закончите покупки напишите "stop")'
   name = gets.chomp.downcase
   break if name == 'stop'
+
   puts 'По какой цене?'
   price = gets.chomp.to_f
   puts 'Кол-во'
-  count = gets.chomp.to_f  
-  cart[name.to_sym] = {price: price, count: count}
+  count = gets.chomp.to_f
+  cart[name.to_sym] = { price: price, count: count }
 end
 
 p cart

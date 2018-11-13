3ac4eb3c6185a7f7c66c6886f8ee018ab2929cc6
VidgarVii <vidasp@mail.ru>
2018-11-13 19:12:13 +0300

    ver. 2.0

--- Base Ruby/lesson_3_OOP/lib/menu/railroad.rb
+++ Base Ruby/lesson_3_OOP/lib/menu/railroad.rb
@@ -13,13 +13,18 @@ class RailRoad
       puts MENU[:main]
       puts '0 - Выйти из программы.'
       choice = gets.chomp.to_i
-      abort if choice.zero?
+      exit?(choice)
       next_menu(choice)
     end
   end
 
   private
-
+  
+  def exit?(choice)
+    abort if choice.zero?
+    start if choice == 9
+  end
+  
   def next_menu(choice)
     case choice
     when 1 then create_menu
@@ -43,10 +48,10 @@ class RailRoad
     end  
     puts 'Список маршрутов:'
     @routes.each_with_index do |route, index| 
-      puts "#{index} - #{route}"
+      puts "#{index} - "
       route.puts_stations
      end     
-    puts 'Список вагонов:'
+    puts "\nСписок вагонов:"
     @wagons.each { |item| puts "- #{item} : #{item.belongs_to}" }
     puts 'Нажмите Enter'
     wait = gets
@@ -56,6 +61,7 @@ class RailRoad
     system('clear')
     puts MENU[:create]
     choice = gets.chomp.to_i
+    exit?(choice)
     case choice
     when 1 then create_station
     when 2 then create_train
@@ -67,12 +73,19 @@ class RailRoad
 
   def route_menu
     system('clear')
+    if @routes.size.zero?
+      puts 'Создайте маршрут'
+      enter = gets
+      start
+    end
+    
     puts MENU[:route]
     choice = gets.chomp.to_i
+    exit?(choice)
     @routes.each_with_index do |route, i|
-      puts "#{i} - #{route.puts_stations}"
+      print "#{i} - "; route.puts_stations
     end
-    puts 'Выберите маршрут'
+    puts "\nВыберите маршрут"
     route = gets.chomp.to_i
     case choice
     when 1 then add_station_route(@routes[route])
@@ -86,21 +99,78 @@ class RailRoad
     end
     puts 'Выберите станцию'
     station = gets.chomp.to_i
-    route.add_station([@stations[station]])
-
+    route.add_station(@stations[station])
   end
 
   def rm_station_route(route)
     route.stations.each_with_index do |station, i|
-      puts 'Выберите станцию'
+      puts "#{i} - #{station.name}"
     end
+    puts 'Выберите станцию'
     station = gets.chomp.to_i
     route.rm_staion(route.stations[station])
   end
 
   def train_menu
     system('clear')
-    puts MENU[:train] 
+    if @trains.size.zero?
+      puts 'Создайте поезд'
+      enter = gets
+      start
+    end
+
+    puts MENU[:train]
+    choice = gets.chomp.to_i
+    exit?(choice)
+    @trains.each_with_index do |train, i|
+      puts "#{i} - #{train.number}:#{train.type}"
+    end
+    puts 'Выберите поезд'
+    train = gets.chomp.to_i
+    case choice
+    when 1 then assign_route_train(@trains[train])
+    when 2 then hook_wagon(@trains[train])
+    when 3 then unhook_wagon(@trains[train])
+    when 4 then move(@trains[train]) 
+    end
+  end
+
+  def assign_route_train(train)
+    puts 'Выберите порядковый номер маршрута'
+    @routes.each_with_index { |route, i| puts "#{i} - #{route}" }
+    route = gets.chomp.to_i
+    train.add_route(@routes[route])
+  end
+
+  def move(train)
+    puts "Куда едим? \n  1 - Вперед\n  2 - Назад"
+    dir = gets.chomp.to_i
+    train.move_forward if dir == 1
+    train.move_back if dir == 2
+  end
+
+  def hook_wagon(train)
+    return puts 'Создайте вагон' if @wagons.size.zero?
+
+    puts 'Выберите порядковый номер вагона'
+    @wagons.each_with_index do |wagon, i|
+      if wagon.belongs_to.nil?
+        puts "#{i} - #{wagon} : #{wagon.type}" 
+      end
+    end
+    wagon = gets.chomp.to_i
+    train.hook_wagon(@wagons[wagon])
+  end
+
+  def unhook_wagon(train)
+    return puts 'Создайте вагон' if @wagons.size.zero?
+
+    puts 'Выберите порядковый номер вагона'
+    train.wagons.each_with_index do |wagon, i|
+      puts "#{i} - #{wagon} : #{wagon.type}"
+    end
+    wagon = gets.chomp.to_i
+    train.unhook_wagon(train.wagons[wagon])
   end
 
   def create_station
@@ -141,8 +211,8 @@ class RailRoad
   end
 
   def create_all
-    @wagon << Wagon.new('passenger')
-    @wagon << Wagon.new('carge')
+    @wagons << Wagon.new('passenger')
+    @wagons << Wagon.new('carge')
     @trains << Train.new('Train 0001', 'passenger')
     @trains << Train.new('Train 0002', 'carge')
     @stations << Station.new('Трансильвания')
@@ -156,5 +226,4 @@ class RailRoad
     puts "Кол-во вагонов: #{count}" 
     puts WAGONS[count]   
   end
-
 end

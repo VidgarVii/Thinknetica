77dc74bdc6bf62076ae99a832219ad8ef1451d59
VidgarVii <vidasp@mail.ru>
2018-11-20 22:59:55 +0300

    seats&volume done

--- Base Ruby/lesson_3_OOP/lib/cargo_wagon.rb
+++ Base Ruby/lesson_3_OOP/lib/cargo_wagon.rb
@@ -1,5 +1,21 @@
 class CargoWagon < Wagon
-  def initialize
-   super('cargo')
+  attr_reader :free_volume, :occupied_volume
+
+  def initialize(volume = 86.4)
+    super('cargo')
+    @free_volume = volume
+    @occupied_volume = 0
+  end
+
+  def take_volume(volume)
+    check_free_volume!(volume)
+    @free_volume = @free_volume - volume
+    @occupied_volume = @occupied_volume + volume
+  end
+
+  private
+
+  def check_free_volume!(volume)
+    raise 'Мест нет' if (@free_volume - volume) <= 0 
   end
 end
--- Base Ruby/lesson_3_OOP/lib/passenger_wagon.rb
+++ Base Ruby/lesson_3_OOP/lib/passenger_wagon.rb
@@ -1,5 +1,21 @@
 class PassengerWagon < Wagon
-  def initialize
+  attr_reader :free_seats, :occupied_seats
+
+  def initialize(seats=30)
     super('passenger')
-  end  
+    @free_seats = seats
+    @occupied_seats = 0    
+  end
+
+  def take_seat
+    check_free_seats!
+    @occupied_seats +=1
+    @free_seats -= 1    
+  end
+  
+  private
+
+  def check_free_seats!
+    raise 'Мест нет' if @free_seats == 0
+  end
 end
